6c057172eccb01a598e4782703849f8c126bbd5b
rkbhowmik <rajib.bhowmik@gmail.com>
2017-10-23 12:46:23 +0200

    - step 1: take file name from the user as a string
    - step 2: Regex for matching patterns and take the amount from Groups[2] and put it in a list array named values
    
    - step 3: Creating a while loop and if loop for rest of the task
    
    - step 4: Done!!

--- SIEReadingFilesApp/Program.cs
+++ SIEReadingFilesApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SIEReadingFilesApp
@@ -13,9 +15,40 @@ namespace SIEReadingFilesApp
 			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("Welcome to the new practice app");
 			Console.ResetColor();
+			Console.WriteLine("Enter your file name with path:");
 
-			Console.WriteLine("Press any key to continue");
-			Console.ReadKey();
+			// Take file name as a string
+			string fileName = Console.ReadLine();
+			StreamReader file = new StreamReader(fileName);
+
+			// declaring variables
+			int counter = 0;
+			string line;
+			//creating a list of array with double data type
+			List<double> values = new List<double>();
+
+			
+			while ((line = file.ReadLine()) != null)
+			{
+				string pattern = @"#TRANS (\d{4}) {.*} (-?\d*.\d*)"; // Example: #TRANS 1630 {} 1.00
+				var match = Regex.Match(line, pattern);
+				if (match.Success)
+				{
+					double converted = Convert.ToDouble(match.Groups[2].Value.Replace('.', ','));
+					values.Add(converted);
+					counter++;
+				}
+			}
+			var sum = values.Sum();
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"Total amount {0}", sum);
+			Console.ResetColor();
+
+			file.Close();
+			Console.WriteLine("There were {0} lines.", counter);
+			
+			// Suspend the screen.  
+			Console.ReadLine();
 
 		}
 	}
